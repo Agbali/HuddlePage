d5ed62c66a7c688af472dae31e937479a81029d0
Blessing Agbali <43254651+Agbali@users.noreply.github.com>
2022-06-22 13:46:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const my_name = 'Blessing Agbali ';
+const my_height = ' 175cm ';
+const my_country = ' Nigeria ';
+
+document.write(my_name);
+document.write(my_height);
+document.write(my_country);
+
+
